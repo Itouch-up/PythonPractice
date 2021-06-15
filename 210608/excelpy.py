5830a726c3bf030090cc4db846f059b92f6a9e7f
Itouch-up <zxcsun1479@outlook.kr>
2021-06-15 17:58:19 +0900

    210615

--- 210608/excelpy.py
+++ 210608/excelpy.py
@@ -1,1 +1,16 @@
 import openpyxl
+
+
+def write_excel_template(filename, sheetname, listdata):
+    excel_file = openpyxl.Workbook()
+    excel_sheet = excel_file.active
+    excel_sheet.column_dimensions['A'].width = 100
+    excel_sheet.column_dimensions['B'].width = 20
+
+    if sheetname != '':
+        excel_sheet.title = sheetname
+
+    for item in listdata:
+        excel_sheet.append(item)
+    excel_file.save(filename)
+    excel_file.close()
